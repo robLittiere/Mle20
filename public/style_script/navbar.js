3cef06fc4947e90c12a8fbc09e39c1a019dc6320
robLittiere <robinl.95@orange.fr>
2021-03-02 22:10:56 +0100

    Navbar + store fix

--- public/style_script/navbar.js
+++ public/style_script/navbar.js
@@ -8,7 +8,9 @@ window.onscroll = function() {myFunction()
 var navbar = document.getElementsByClassName("navbar")[0];
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop + 160;
+
+
 
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
