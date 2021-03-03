d4b24803bda82c2096a333cd5fe7fe29618c4f3b
robLittiere <robin.littiere@edu.itescia.fr>
2021-03-03 17:05:07 +0100

    Added body "Accueil"

--- public/style_script/slideshow.js
+++ public/style_script/slideshow.js
@@ -1,10 +1,26 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '.splide', {
-        type       : 'fade',
+    let splide = new Splide( '.splide', {
+        type : 'loop',
+        drag: true,
+        autoplay: true,
+        pauseOnHover: false,
+        interval: 5000,
+        speed: 400,
         heightRatio: 0.4,
         cover      : true,
         rewind: true,
+        classes: {
+            arrows: 'splide__arrows your-class-arrows',
+            arrow : 'splide__arrow your-class-arrow',
+            prev  : 'splide__arrow--prev prev__arrow',
+            next  : 'splide__arrow--next next__arrow',
+        },
 
-    } ).mount();
-} );
+    });
+
+    splide.mount();
+    
+});
+
+
 
