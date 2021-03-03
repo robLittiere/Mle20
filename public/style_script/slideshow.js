7035087e540617d9f9d15a2abe0a34004e2f9451
robLittiere <robin.littiere@edu.itescia.fr>
2021-03-03 11:57:57 +0100

    slidershow added feature

---
+++ public/style_script/slideshow.js
@@ -0,0 +1,10 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide', {
+        type       : 'fade',
+        heightRatio: 0.4,
+        cover      : true,
+        rewind: true,
+
+    } ).mount();
+} );
+
