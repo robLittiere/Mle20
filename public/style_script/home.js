31ceb59a041652f28d29ffc3517d1531040b4719
Mancone Alexis <ale.mancone@gmail.com>
2021-03-04 15:34:16 +0100

    Fix popup and style modifications

--- public/style_script/home.js
+++ public/style_script/home.js
@@ -9,14 +9,32 @@ let buttonOver = document.getElementById("over18Years")
 // When the user load the page, open the modal 
 window.onload = function () {
   modal.style.display = "block";
+  disableScroll();
 }
 
 // When the user clicks on over18Years button, close the modal
 buttonOver.onclick = function () {
   modal.style.display = "none";
+  enableScroll();
 }
 
 //When the user clicks on under18Years button, leave the page
 buttonUnder.onclick = function () {
     window.open("https://www.google.com")
 }
+
+//Function to disable scrolling
+function disableScroll() { 
+  // Get the current page scroll position 
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
+
+      // if any scroll is attempted, set this to the previous value 
+      window.onscroll = function() { 
+        window.scrollTo(scrollLeft, scrollTop); 
+      }; 
+} 
+//Function to enable scrolling
+function enableScroll() { 
+  window.onscroll = function() {}; 
+} 
